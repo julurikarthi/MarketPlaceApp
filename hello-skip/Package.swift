917d2738b3fee5a637a81ffbfa8011af17776dc4
karthik juluri <karthik.juluri@publix.com>
2025-01-18 16:41:18 -0500

    create project

---
+++ hello-skip/Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version: 5.9
+// This is a Skip (https://skip.tools) package,
+// containing a Swift Package Manager project
+// that will use the Skip build plugin to transpile the
+// Swift Package, Sources, and Tests into an
+// Android Gradle Project with Kotlin sources and JUnit tests.
+import PackageDescription
+
+let package = Package(
+    name: "hello-skip",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v17), .macOS(.v14), .tvOS(.v17), .watchOS(.v10), .macCatalyst(.v17)],
+    products: [
+        .library(name: "HelloSkipApp", type: .dynamic, targets: ["HelloSkip"]),
+    ],
+    dependencies: [
+        .package(url: "https://source.skip.tools/skip.git", from: "1.2.21"),
+        .package(url: "https://source.skip.tools/skip-ui.git", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "HelloSkip", dependencies: [
+            .product(name: "SkipUI", package: "skip-ui")
+        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
+        .testTarget(name: "HelloSkipTests", dependencies: [
+            "HelloSkip",
+            .product(name: "SkipTest", package: "skip")
+        ], resources: [.process("Resources")], plugins: [.plugin(name: "skipstone", package: "skip")]),
+    ]
+)
